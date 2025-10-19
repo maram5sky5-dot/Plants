c47940060079d690d67cb0c8bb336c7f1a108a22
maram <ma.ram.5.sky5@gmail.com>
2025-10-19 10:18:25 +0300

    Initial Commit

---
+++ Plants/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plants
+//
+//  Created by Maram Ibrahim  on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plants/PlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsApp.swift
+//  Plants
+//
+//  Created by Maram Ibrahim  on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
